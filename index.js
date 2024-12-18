3795a1702863a7a7534f9c19cceb3cce1820fbc9
kukuh-imanura <kukuh.imanura.bagaskara@gmail.com>
2024-12-18 15:56:34 +0800

    add cors

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ import routes from './api/routes/routes.js';
 const app = express();
 const port = 3000;
 
-app.use(express.json());
 app.use(cors());
-
+app.use(express.json());
 app.use(routes);
 
 app.get('/', (req, res) => {
