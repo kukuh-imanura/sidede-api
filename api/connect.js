6f36c52b5651c535ad985731eaeaaf33e009a433
kukuh-imanura <kukuh.imanura.bagaskara@gmail.com>
2024-12-18 13:26:01 +0800

    update db

--- api/connect.js
+++ api/connect.js
@@ -1,9 +1,6 @@
 import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
-dotenv.config();
 
 const pool = mysql.createPool({
-  // UNTUK LOKAL
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
