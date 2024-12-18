fa97f276bf61323d061b0d79583c818d9a47e5e2
kukuh-imanura <kukuh.imanura.bagaskara@gmail.com>
2024-12-18 13:40:00 +0800

    update hosting

--- api/index.js
+++ api/index.js
@@ -8,6 +8,10 @@ app.use(express.json());
 
 app.use(routes);
 
+app.get('/', (req, res) => {
+  res.send('Welcome to SIDEDE API');
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`);
 });
