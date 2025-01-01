49522f1138289e2e7667ecca2bdd91c39123df11
kukuh-imanura <kukuh.imanura.bagaskara@gmail.com>
2025-01-01 15:48:41 +0800

    update auth

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -7,6 +7,7 @@ import pemeriksaan from './Pemeriksaan.js';
 import mobileunit from './MobileUnit.js';
 import pertanyaan from './Pertanyaan.js';
 import screening from './Screening.js';
+import auth from './Auth.js';
 
 const app = express();
 
@@ -17,5 +18,6 @@ app.use('/pemeriksaan', pemeriksaan);
 app.use('/mobileunit', mobileunit);
 app.use('/pertanyaan', pertanyaan);
 app.use('/screening', screening);
+app.use('/auth', auth);
 
 export default app;
